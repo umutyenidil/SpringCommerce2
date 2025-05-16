5cfe39e1771588062a9cc891bff64ba0ea7e4070
Umut Yenidil <umut.yenidil9@gmail.com>
2025-05-16 20:51:19 +0300

    product olusturma test edildi

--- product-service/src/test/java/com/umutyenidil/springcommerce2/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/umutyenidil/springcommerce2/ProductServiceApplicationTests.java
@@ -1,15 +1,60 @@
 package com.umutyenidil.springcommerce2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @LocalServerPort
+    private Integer port;
+
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @BeforeEach
+    void setUp() {
+
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+
+        mongoDBContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+
+        String requestBody = """
+                {
+                    "name": "Apple Macbook",
+                    "description": "Apple Macbook Pro",
+                    "price": 100.0
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/v1/products")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Apple Macbook"))
+                .body("description", Matchers.equalTo("Apple Macbook Pro"))
+                .body("price", Matchers.equalTo(100.0f));
     }
 
 }
